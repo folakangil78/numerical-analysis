5385f19581e92891d4bc2325d3cb6a33392f83d5
Francis Olakangil <fj.olakangil@gmail.com>
2026-02-20 11:06:50 -0500

    2d version, func defs

---
+++ newton_method_2D.m
@@ -0,0 +1,15 @@
+clc; clear; close all;
+
+%% ---- Define the nonlinear system f(x,y) ----
+% Replace these with YOUR functions from parts (a)-(c)
+
+f = @(x) [ ...
+    x(1)^2 + x(2)^2 - 4;          % f1(x,y)
+    x(1) - x(2) - 1               % f2(x,y)
+];
+
+% Jacobian matrix
+J = @(x) [ ...
+    2*x(1), 2*x(2);               % df1/dx , df1/dy
+    1,      -1                    % df2/dx , df2/dy
+];
