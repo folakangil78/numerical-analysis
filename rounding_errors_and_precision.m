10968b6d94243cb02222464c1fc801bf04052959
Francis Olakangil <fj.olakangil@gmail.com>
2026-02-20 11:30:07 -0500

    finite precision practice, expected values

---
+++ rounding_errors_and_precision.m
@@ -0,0 +1,9 @@
+% Set the format to long to display more decimal places, 
+% though we will use fprintf for specific precision.
+format long;
+
+% Part (a): Evaluating variables 'a' and 'b'
+
+% 1. Evaluate 'a'
+a_computer = (1 - 1) + 10^-16;
+a_exact = 10^-16; % Analytical result
