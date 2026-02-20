871df229f54cda80a23130ea9d63e4c7e3cda79b
Francis Olakangil <fj.olakangil@gmail.com>
2026-02-20 11:30:36 -0500

    outputs

--- rounding_errors_and_precision.m
+++ rounding_errors_and_precision.m
@@ -7,3 +7,7 @@ format long;
 % 1. Evaluate 'a'
 a_computer = (1 - 1) + 10^-16;
 a_exact = 10^-16; % Analytical result
+
+fprintf('--- Results for a ---\n');
+fprintf('Analytical exact result: %2.20f\n', a_exact);
+fprintf('Computer result:         %2.20f\n\n', a_computer);
