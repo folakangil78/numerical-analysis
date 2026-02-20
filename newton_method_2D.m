3b028e1d948225193fef4d924dab33a2eca5df25
Francis Olakangil <fj.olakangil@gmail.com>
2026-02-20 11:07:34 -0500

    params and starting pts

--- newton_method_2D.m
+++ newton_method_2D.m
@@ -13,3 +13,10 @@ J = @(x) [ ...
     2*x(1), 2*x(2);               % df1/dx , df1/dy
     1,      -1                    % df2/dx , df2/dy
 ];
+
+%% ---- Newton parameters ----
+maxIter = 5;
+
+% Starting points
+x0_1 = [2; 3];
+x0_2 = [-1.5; 2];
