a9870c536d10a6387c878a9bd146cef1693b5e14
Francis Olakangil <fj.olakangil@gmail.com>
2026-03-06 10:12:53 -0500

    built in matlab norm comparison

--- matrix_norms.m
+++ matrix_norms.m
@@ -34,3 +34,25 @@ myTimes = zeros(1,8);
 builtinTimes = zeros(1,8);
 
 n = 100;
+
+for k = 1:8
+    
+    sizes(k) = n;
+    
+    A = rand(n,n);
+    
+    % my implem
+    tic
+    norm1 = max(sum(abs(A),1));
+    normInf = max(sum(abs(A),2));
+    myTimes(k) = toc;
+    
+    % MATLAB implem
+    tic
+    n1 = norm(A,1);
+    nInf = norm(A,inf);
+    builtinTimes(k) = toc;
+    
+    n = 2*n;
+
+end
