ddfe30848ac07bb7b9aa1a699a5398a6034ecfe9
Francis Olakangil <fj.olakangil@gmail.com>
2026-03-06 10:09:27 -0500

    timing experiment, no loops

--- matrix_norms.m
+++ matrix_norms.m
@@ -4,3 +4,26 @@ norm1 = max(sum(abs(A),1));     % column sums
 normInf = max(sum(abs(A),2));   % row sums
 
 end
+
+sizes = zeros(1,8);
+times = zeros(1,8);
+
+n = 100;
+
+for k = 1:8
+    
+    sizes(k) = n;
+    
+    A = rand(n,n);
+    
+    tic
+    norm1 = max(sum(abs(A),1));
+    normInf = max(sum(abs(A),2));
+    times(k) = toc;
+    
+    n = 2*n;
+
+end
+
+disp([sizes' times'])
+
