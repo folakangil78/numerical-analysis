6ad697583051c80b12ca54d15ae5a51e966fc7eb
Francis Olakangil <fj.olakangil@gmail.com>
2026-04-17 11:47:18 -0400

    construct stochastic

---
+++ (2)google_page_rank.py
@@ -0,0 +1,8 @@
+import numpy as np
+import matplotlib.pyplot as plt
+
+# ---------------------------------------------------------
+# Setup: Constructing the stochastic matrix L (from prompt)
+# ---------------------------------------------------------
+np.random.seed(42) # For reproducible results
+n = 100 # Pick a size n >= 100 for the "toy internet"
