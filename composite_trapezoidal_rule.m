135d5083050b647e4d09acd5e147b537aa430d0d
Francis Olakangil <fj.olakangil@gmail.com>
2026-05-04 22:14:17 -0400

    error calculation

--- composite_trapezoidal_rule.m
+++ composite_trapezoidal_rule.m
@@ -20,3 +20,16 @@ f      = @(x) sqrt(x);                  % integrand
 a      = 0.1;                           % lower limit
 b      = 1.0;                           % upper limit
 I_exact = 2/3 - 1/(15*sqrt(10));        % exact value (given)
+
+m_list = [10 20 40 80 160 320 640 1280];   % subinterval counts to test
+errors_a = zeros(size(m_list));
+ 
+fprintf('   m         T_m            |error|\n');
+fprintf('   --        ----           -------\n');
+for k = 1:length(m_list)
+    m              = m_list(k);
+    T_m            = trapez(f, a, b, m);
+    errors_a(k)    = abs(T_m - I_exact);
+    fprintf('  %4d   %.10f   %.3e\n', m, T_m, errors_a(k));
+end
+fprintf('\n');
